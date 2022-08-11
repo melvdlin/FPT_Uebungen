76b1d78c72e270e83125552aa4d52c5e4c75e56b
melvdl <hansw533@gmail.com>
2022-08-11 12:57:07 +0200

    simplify fizzbuzz

--- uebung/_1/src/org/somevand/fpt/teaching/uebung/_1/FizzBuzz.java
+++ uebung/_1/src/org/somevand/fpt/teaching/uebung/_1/FizzBuzz.java
@@ -1,20 +1,21 @@
 package org.somevand.fpt.teaching.uebung._1;
 
 public class FizzBuzz {
-    public static void fizz(int i) {
-        if (i % 3 == 0) System.out.print("Fizz");
-    }
+    static String fizzbuzz(int i) {
+        String out = "";
 
-    public static void buzz(int i) {
-        if (i % 5 == 0) System.out.print("Buzz");
+        if (i % 3 == 0)
+            out += "Fizz";
+        if (i % 5 == 0)
+            out += "Buzz";
+
+        return out;
     }
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.print(i + ": ");
-            fizz(i);
-            buzz(i);
-            System.out.println();
+            System.out.println(fizzbuzz(i));
         }
     }
 }
