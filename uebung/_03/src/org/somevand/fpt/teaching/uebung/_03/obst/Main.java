dc8d3261dceded2b0a9c5102d897d13e0dc065f9
melvdl <hansw533@gmail.com>
2022-10-18 11:57:28 +0200

    fix indentation in obst main class

--- uebung/_03/src/org/somevand/fpt/teaching/uebung/_03/obst/Main.java
+++ uebung/_03/src/org/somevand/fpt/teaching/uebung/_03/obst/Main.java
@@ -7,6 +7,6 @@ public class Main {
         Watermelon melon2 = new Watermelon(12345678, 4.2, Color.GREEN, "Duisburg", 0.99F);
         System.out.println("melon1.equals(melon2)   == " + melon1.equals(melon2));    // true
         System.out.println("melon2.equals(melon1)   == " + melon2.equals(melon1));    // true
-        System.out.println("(melon1 == melon2)      == " + (melon1 == melon2));          // false
+        System.out.println("(melon1 == melon2)      == " + (melon1 == melon2));       // false
     }
 }
